6d15917267e310086ef3c9b4562eff7240e1a373
Eniola Hannah <heniola395@gmail.com>
2022-09-28 13:02:14 +0100

    Did the responsiveness

--- script.js
+++ script.js
@@ -3,29 +3,29 @@ let f9Song = new Audio("gradingSystemPlaylist/QBEGN9A-game-lose.mp3")
 
 const showResult=()=>{
     score = userInput.value
-    if(score >= 0 && score < 40){
-        userOutput.innerText = "F9 ~ Failed"
+    if (score == ""){
+        userDisplay.innerText = "Input a score"
+    }
+    else if(score >= 0 && score < 40){
+        userDisplay.innerText = "F9 ~ Failed"
         f9Song.play()
     }
     else if(score >= 40 && score < 45){
-        userOutput.innerText = "D7 ~ Passed"
+        userDisplay.innerText = "D7 ~ Passed"
     }
     else if (score >= 45 && score < 50){
-        userOutput.innerText = "C4 ~ Credit"
+        userDisplay.innerText = "C4 ~ Credit"
     }
     else if (score >= 50 && score < 60){
-        userOutput.innerText = "B3 ~ Average"
+        userDisplay.innerText = "B3 ~ Average"
     }
     else if (score >= 60 && score < 75){
-        userOutput.innerText = "B2 ~ Distinctive"
+        userDisplay.innerText = "B2 ~ Distinctive"
     }
     else if(score >= 75 && score <= 100){ 
-        userOutput.innerText = "A1 ~ Excellent"
+        userDisplay.innerText = "A1 ~ Excellent"
         a1Song.play()
-    }
-    else if (score == ""){
-        userOutput.innerText = "Input a score"
     }else{
-        userOutput.innerText = "Undefined"
+        userDisplay.innerText = "Undefined"
     }
 }
